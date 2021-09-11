b86e83de78dd0d56ad69334364fb209285d4c965
KIARA SMALL <SMALLKAY1726@GMAIL.COM>
2021-09-10 20:23:15 -0500

    added changes

--- cw-main.js
+++ cw-main.js
@@ -2,24 +2,24 @@
 // Create a class Product that represents a product sold online. A product has a price, quantity and name.
 //The class should have a method that prints a product's information in the following format: Banana, price 1.1, amount 13.
 //Create two instances of the class and call the method created to print the product's details.
-class Product
-{
-    constructor(price, quantity, name)
-    {
-        this.price = price;
-        this.quantity = quantity;
-        this.name = name;
-    };
-    printAllProducts()
-    {
-        alert(`Name: ${this.name}, Price: ${this.price}, Quantity: ${this.quantity}.`)
-    };
-}
+// class Product
+// {
+//     constructor(name, price, quantity)
+//     {
+//         this.name = name;
+//         this.price = price;
+//         this.quantity = quantity;
+//     };
+//     printAllProducts()
+//     {
+//         alert(`Name: ${this.name}, Price: ${this.price}, Quantity: ${this.quantity}.`)
+//     };
+// }
 
-let product1 = new Product("Steak", "price 10","amount 2");
-let product2 = new Product("pasta","price 2.5", "amount 1");
-product1.printAllProducts();
-product2.printAllProducts();
+// let product1 = new Product("Steak", "price 10","amount 2");
+// let product2 = new Product("pasta","price 2.5", "amount 1");
+// product1.printAllProducts();
+// product2.printAllProducts();
 
 
 // Problem 2:
@@ -30,6 +30,48 @@ product2.printAllProducts();
 //Create an instance of the Pet Class called pet1. Print all property values of the object using the class method. 
 //Create another instance of the Pet Class with diffrent property values called pet2. Print all property values of the object using the class method. Change the age grade of pet1. 
 //Print the age of pet1 in dog years using the class method. Print the age of pet2 in dog years using the class method
+// class Pet
+// {
+//     constructor(type, breed, age)
+//     {
+//         this.type = type;
+//         this.breed = breed;
+//         this.age = age;
+//     };
+//     formatProperties()
+//     {
+//         alert(`Type: ${this.type}, Breed: ${this.breed}, Age: ${this.age}.`)
+//     };
+//     changeAge(number)
+//     {
+//         this.age = parseFloat(this.age);
+//         this.age = number;
+//         console.log(`${this.type} is now ${this.age} yeaars old!!`);
+//     };
+//     dogYears()
+//     {
+//         if(this.type === "dog")
+//         {
+//             this.age = parseFloat(this.age);
+//             return console.log(`${this.type} is ${this.age * 7} in dog years`);
+//         }
+//         else
+//         {
+//             console.log(`${this.type} is not a dog`)
+//         }
+//     }
+// }
+
+// let pet1 = new Pet("dog","Chow Chow","4");
+// pet1.formatProperties();
+// pet1.changeAge(8);
+// pet1.dogYears();
+
+// let pet2 = new Pet("dog","Pit Bull", "1");
+// pet2.formatProperties();
+// pet2.changeAge(5);
+// pet2.dogYears();
+
 
 // Problem 3: Inheritance
 // Create a Person Class with the properties Name (first, last), Age, Interests, Bio ('NAME is AGE years old. They like INTERESTS')
